40b94be41608e3a0433121df859bbc6568664fc1
Nick Lanzo <nlanzo@gmail.com>
2024-04-30 18:19:55 -0500

    fix: made date parameter optional

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date?", function (req, res) {
   const inputDate = req.params.date;
   let date;
 
